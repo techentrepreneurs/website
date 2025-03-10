ab6f0b0ed777ff4ab5b5d828ef7dc634221ba236
dmarcotrigiano <daniel@three.systems>
2025-03-09 22:40:14 -0400

    Update project name and configure image optimization settings

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+
   /* config options here */
   images: {
     domains: ['i.imgur.com', 'images.unsplash.com'],
+    unoptimized: true
   },
 };
 
