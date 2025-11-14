fed4e16076ae3291ae0af27c3eb44e86d011680d
n3s-online <n3s.online@gmail.com>
2025-11-13 18:02:14 -0800

    swap out opennext with next

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   images: {
     domains: ["i.imgur.com", "images.unsplash.com", "www.google.com"],
-    unoptimized: true,
   },
 };
 
 export default nextConfig;
-
-import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
-initOpenNextCloudflareForDev();
--- open-next.config.ts
+++
@@ -1,5 +0,0 @@
-import { defineCloudflareConfig } from "@opennextjs/cloudflare";
-
-export default defineCloudflareConfig({
-  // Using default cache (no R2 for now - can enable later)
-});
